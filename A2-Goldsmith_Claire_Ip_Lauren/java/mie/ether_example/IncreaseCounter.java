93471fc865de48159513d2b5756720f51d0d3be8
laurenip <lauren.ip@outlook.com>
2017-11-18 16:53:17 +0000

    hello

---
+++ A2-Goldsmith_Claire_Ip_Lauren/java/mie/ether_example/IncreaseCounter.java
@@ -0,0 +1,62 @@
+package mie.ether_example;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+import java.security.spec.ECGenParameterSpec;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.DynamicArray;
+import org.web3j.abi.datatypes.generated.Bytes32;
+import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.ECKeyPair;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.request.Transaction;
+import org.web3j.protocol.core.methods.response.EthCoinbase;
+import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
+import org.web3j.protocol.core.methods.response.EthSendTransaction;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.protocol.parity.Parity;
+import org.web3j.protocol.parity.methods.response.PersonalUnlockAccount;
+import org.web3j.tx.ClientTransactionManager;
+import org.web3j.tx.Transfer;
+import org.web3j.utils.Numeric;
+
+import edu.toronto.dbservice.config.MIE354DBHelper;
+import edu.toronto.dbservice.types.EmployeeVote;
+import edu.toronto.dbservice.types.EtherAccount;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+import org.bouncycastle.crypto.generators.ECKeyPairGenerator;
+import org.bouncycastle.crypto.params.ECKeyGenerationParameters;
+
+public class IncreaseCounter implements JavaDelegate{
+	
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		
+		int currentValue = (int) execution.getVariable("tokenCounter");
+		execution.setVariable("tokenCounter", currentValue+1);
+		
+	}
+
+}
---
+++ A2-Goldsmith_Claire_Ip_Lauren/java/mie/ether_example/InitializeCounter.java
@@ -0,0 +1,56 @@
+package mie.ether_example;
+
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+import java.security.spec.ECGenParameterSpec;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.web3j.abi.datatypes.Address;
+import org.web3j.abi.datatypes.DynamicArray;
+import org.web3j.abi.datatypes.generated.Bytes32;
+import org.web3j.abi.datatypes.generated.Uint256;
+import org.web3j.crypto.Credentials;
+import org.web3j.crypto.ECKeyPair;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.request.Transaction;
+import org.web3j.protocol.core.methods.response.EthCoinbase;
+import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
+import org.web3j.protocol.core.methods.response.EthSendTransaction;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
+import org.web3j.protocol.http.HttpService;
+import org.web3j.protocol.parity.Parity;
+import org.web3j.protocol.parity.methods.response.PersonalUnlockAccount;
+import org.web3j.tx.ClientTransactionManager;
+import org.web3j.tx.Transfer;
+import org.web3j.utils.Numeric;
+
+import edu.toronto.dbservice.types.EtherAccount;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+import org.bouncycastle.crypto.generators.ECKeyPairGenerator;
+import org.bouncycastle.crypto.params.ECKeyGenerationParameters;
+
+public class InitializeCounter implements JavaDelegate{
+
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		
+		execution.setVariable("tokenCounter", 10);
+		
+	}
+}
