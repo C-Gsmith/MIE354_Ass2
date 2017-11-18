2da1e5890b101385454c9b869d6ad20ffda215d8
laurenip <lauren.ip@outlook.com>
2017-11-18 17:02:35 +0000

    changes

---
+++ A2-Goldsmith_Claire_Ip_Lauren/src/test/java/mie/ether_example/MOutOfNUnitTest.java
@@ -0,0 +1,35 @@
+package mie.ether_example;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.TaskService;
+import org.activiti.engine.form.FormProperty;
+import org.activiti.engine.form.TaskFormData;
+import org.activiti.engine.task.Task;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MOutOfNUnitTest extends LabBaseUnitTest {
+	
+	@BeforeClass
+	public static void setupFile() {
+		filename = "src/main/resources/diagrams/MOutOfN.bpmn";
+	}
+	
+	private void startProcess() {	
+		RuntimeService runtimeService = activitiContext.getRuntimeService();
+		processInstance = runtimeService.startProcessInstanceByKey("myProcess");
+	}
+	
+	@Test
+	public void runProcess() {
+		startProcess();
+	}
+	
+}
